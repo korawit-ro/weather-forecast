3eb1640fbc4e22ba99accbc0662e74df7ef860dd
Korawit Romsuk <korawit.ro@defenseme.com>
2023-03-24 09:20:05 +0700

    feat(1.0.0): implement dark sky icon mapped

--- src/lib/weather.helper.ts
+++ src/lib/weather.helper.ts
@@ -4,6 +4,7 @@ import timezone from "dayjs/plugin/timezone";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import { WeatherAPICom, Hour } from "./weather-apicom.dto";
 import { DarkSkyAPI } from "./weather-darksky.dto";
+import { MappedDarkSkyIcon } from "./weather.icon";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -13,6 +14,22 @@ export function toDarkSkyAPI(weatherData: WeatherAPICom): DarkSkyAPI {
   const fCurrent = weatherData.forecast.forecastday[0];
   const fTomorrow = weatherData.forecast.forecastday[1];
   const currentDayjs = dayjs().tz(weatherData.location.tz_id);
+
+  const sunriseTime = dayjs
+    .tz(
+      `${fCurrent.date} ${fCurrent.astro.sunrise}`,
+      "YYYY-MM-DD hh:mm A",
+      weatherData.location.tz_id
+    )
+    .unix();
+  const sunsetTime = dayjs
+    .tz(
+      `${fCurrent.date} ${fCurrent.astro.sunset}`,
+      "YYYY-MM-DD hh:mm A",
+      weatherData.location.tz_id
+    )
+    .unix();
+
   let selectIndex: number = fCurrent.hour.findIndex(
     (hData) => currentDayjs.unix() < hData.time_epoch
   );
@@ -22,8 +39,6 @@ export function toDarkSkyAPI(weatherData: WeatherAPICom): DarkSkyAPI {
     selectIndex = 0;
   }
   const selectHour: Hour = fCurrent.hour[selectIndex];
-
-  
 
   let c = 0;
   const hourList = fCurrent.hour.slice(selectIndex, selectIndex + 7);
@@ -43,7 +58,7 @@ export function toDarkSkyAPI(weatherData: WeatherAPICom): DarkSkyAPI {
       time: currentDayjs.unix(),
       timeTxt: currentDayjs.tz(weatherData.location.tz_id).format(),
       summary: selectHour.condition.text,
-      icon: selectHour.condition.icon,
+      icon: MappedDarkSkyIcon(selectHour.condition.code, currentDayjs.unix(), sunriseTime, sunsetTime),
       newIcon: selectHour.condition.icon,
       temperature: selectHour.temp_c,
       humidity: selectHour.humidity,
@@ -55,9 +70,12 @@ export function toDarkSkyAPI(weatherData: WeatherAPICom): DarkSkyAPI {
       newIcon: weatherData.current.condition.icon,
       data: hourList.map((hData) => ({
         time: hData.time_epoch,
-        timeTxt: dayjs.unix(hData.time_epoch).tz(weatherData.location.tz_id).format(),
+        timeTxt: dayjs
+          .unix(hData.time_epoch)
+          .tz(weatherData.location.tz_id)
+          .format(),
         summary: hData.condition.text,
-        icon: hData.condition.icon,
+        icon: MappedDarkSkyIcon(hData.condition.code, hData.time_epoch, sunriseTime, sunsetTime),
         newIcon: hData.condition.icon,
         temperature: hData.temp_c,
         humidity: hData.humidity,
@@ -66,24 +84,12 @@ export function toDarkSkyAPI(weatherData: WeatherAPICom): DarkSkyAPI {
     },
     daily: {
       summary: weatherData.current.condition.text,
-      icon: weatherData.current.condition.icon,
+      icon: MappedDarkSkyIcon(weatherData.current.condition.code, currentDayjs.unix(), sunriseTime, sunsetTime),
       newIcon: weatherData.current.condition.icon,
       data: [
         {
-          sunriseTime: dayjs
-            .tz(
-              `${fCurrent.date} ${fCurrent.astro.sunrise}`,
-              "YYYY-MM-DD hh:mm A",
-              weatherData.location.tz_id
-            )
-            .unix(),
-          sunsetTime: dayjs
-            .tz(
-              `${fCurrent.date} ${fCurrent.astro.sunset}`,
-              "YYYY-MM-DD hh:mm A",
-              weatherData.location.tz_id
-            )
-            .unix(),
+          sunriseTime,
+          sunsetTime,
           temperatureMin: fCurrent.day.mintemp_c,
           temperatureMax: fCurrent.day.maxtemp_c,
         },
